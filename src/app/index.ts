5ecbdd447553e5548d45f71251638e3dc253def7
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-02-16 21:37:32 +0530

    adding-tweet-feed-schema

--- src/app/index.ts
+++ src/app/index.ts
@@ -6,6 +6,7 @@ import { expressMiddleware } from "@apollo/server/express4";
 import { User } from "./user";
 import { GraphqlContext } from "../interfaces";
 import JWTService from "../services/jwt";
+import { Tweet } from "./tweet";
 
 export async function initServer() {
   const app = express();
@@ -16,13 +17,25 @@ export async function initServer() {
   const graphQLServer = new ApolloServer<GraphqlContext>({
     typeDefs: `
       ${User.types}
+      ${Tweet.types}
       type Query {
         ${User.queries}
+        ${Tweet.queries}
+      }
+      type Mutation {
+        ${Tweet.muatations}
+      }
     }`,
     resolvers: {
       Query: {
         ...User.resolvers.queries,
+        ...Tweet.resolvers.queries,
       },
+      Mutation: {
+        ...Tweet.resolvers.mutations,
+      },
+      ...Tweet.resolvers.extraResolvers,
+      ...User.resolvers.extraResolvers,
     },
   });
 
@@ -34,7 +47,9 @@ export async function initServer() {
       context: async ({ req, res }) => {
         return {
           user: req.headers.authorization
-            ? JWTService.decodeToken(req.headers.authorization.split("Bearer ")[1])
+            ? JWTService.decodeToken(
+                req.headers.authorization.split("Bearer ")[1]
+              )
             : undefined,
         };
       },
